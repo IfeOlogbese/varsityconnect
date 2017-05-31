c152e806ca873b6dee6ad650efb4f2458c5f32d5
IfeOlogbese <ologbesei@yahoo.com>
2017-05-31 13:52:49 +0200

    display all users

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,6 +1,10 @@
 class Users::SessionsController < Devise::SessionsController
   # before_action :configure_sign_in_params, only: [:create]
 
+  def index
+    @users = User.all
+  end
+  
   # GET /resource/sign_in
   # def new
   #   super
