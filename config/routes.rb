dc8922dfa56ca113aecfbd74f1bc4206ae2dc24f
IfeOlogbese <ologbesei@yahoo.com>
2017-05-31 13:49:24 +0200

    set route url to post#index and set users_path

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,17 @@ Rails.application.routes.draw do
   devise_for :users, controllers: {
     sessions: 'users/sessions'
   }
+
+  # custom user routes
+  # Reference link --> https://blog.payload.tech/2016/03/18/custom-routes-and-views-with-devise/
+  # --->  http://ricostacruz.com/cheatsheets/devise.html
+  # --->  http://api.rubyonrails.org/classes/ActionDispatch/Routing.html
+  as :user do
+    get "/users" => 'users/sessions#index'
+  end
+
   get 'home/front'
   
-  root to: 'home#front'
+  root to: 'posts#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
