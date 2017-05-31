d5445e33a8d51c6d16039f7f5a2584ae9a5fa7b7
IfeOlogbese <ologbesei@yahoo.com>
2017-05-31 13:51:28 +0200

    set relationship to belong to user

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,7 +2,5 @@ class Post
   include Mongoid::Document
   field :attachment, type: String
   field :content, type: String
-  embedded_in :user
-
-  mount_uploader :attachment, AvatarUploader
+  belongs_to :user
 end
